71af1f4b464d8d136d302cca711f786d97fde77a
Tikaram Mardi <tikarammardi007@gmail.com>
2017-12-16 18:35:57 +0530

    1 prj added

---
+++ Clock/js/clock.js
@@ -0,0 +1,37 @@
+var d,h,m,s,clock,clr;
+
+function myClock(){
+    d =new Date();
+    h = d.getHours();
+    m = d.getMinutes();
+    s = d.getSeconds();
+
+
+if(h<=9){
+    h = '0' + h;
+    
+}
+
+if(m<=9){
+    m = '0' + m;
+    
+}
+
+
+if(s<=9){
+    s = '0' + s;
+    
+}
+
+clock = h + ":" + m + ":" + s;
+
+        
+clr = "#" + h + m + s;
+    
+setTimeout(myClock,1000);
+document.getElementById("clock").innerHTML = clock;
+document.body.style.background =clr;
+    
+}
+
+myClock();
